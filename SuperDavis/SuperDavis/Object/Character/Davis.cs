df2d87806073bed8755da555fe26136e7a170d65
xu.2460 <xu.2460@osu.edu>
2019-06-06 13:25:57 -0400

    comment out some useless stuff - for refactoring

--- SuperDavis/SuperDavis/Object/Character/Davis.cs
+++ SuperDavis/SuperDavis/Object/Character/Davis.cs
@@ -18,7 +18,7 @@ namespace SuperDavis.Object.Character
             DavisStatus = DavisStatus.Davis;
             DavisState = new DavisStaticRightState(this);
             Location = location;
-            HitBox = new Rectangle((int)Location.X, (int)Location.Y, DavisState.Width, DavisState.Height);
+            //HitBox = new Rectangle((int)Location.X, (int)Location.Y, DavisState.Width, DavisState.Height);
     }
 
         public void Update(GameTime gameTime)
