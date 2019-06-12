de63213cad5da4c7241de4f8c9faf3ad3d310156
xu.2460 <xu.2460@osu.edu>
2019-06-12 13:15:40 -0400

    ss

---
+++ SuperDavis/SuperDavis/Variables/Variable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperDavis.Variables
+{
+    public static class Variable
+    {
+        public static int  WindowsEdgeWidth = 1024;
+        public static int WindowsEdgeHeight = 768;
+    }
+}
