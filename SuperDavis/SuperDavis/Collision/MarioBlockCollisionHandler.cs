b476a99a5769341a75a7ca0c530f53428e0f3e20
Ryan <Ryan@DESKTOP-CS6SR8V>
2019-06-11 22:48:47 -0400

    Working on hidden block feature.  In progress still.

--- SuperDavis/SuperDavis/Collision/MarioBlockCollisionHandler.cs
+++ SuperDavis/SuperDavis/Collision/MarioBlockCollisionHandler.cs
@@ -16,32 +16,49 @@ namespace SuperDavis.Collision
         
         public static void HandleCollision(IDavis davis, IBlock block,CollisionSide side)
         {
+            bool isVisible = false;
             // Location setup
-            switch(side)
+            switch (side)
             {
+                case CollisionSide.Bottom:
+                    davis.Location = new Vector2(davis.Location.X, block.Location.Y + block.HitBox.Height);
+                    if (block is HiddenBlock)
+                    {
+                        isVisible = true;
+                        block.SpecialState();
+                    }
+                    else if (block is QuestionBlock)
+                    {
+                        block.SpecialState();
+                    }
+                    else if (block is Brick)
+                    {
+                        block.SpecialState();
+                        block.Remove = true;
+                    }
+                    break;
                 case CollisionSide.Top:
-                    davis.Location = new Vector2(davis.Location.X, block.Location.Y - davis.HitBox.Height);
+                    //if not hidden block
+                    if (!(block is HiddenBlock))
+                    {
+                        davis.Location = new Vector2(davis.Location.X, block.Location.Y - davis.HitBox.Height);
+                    }
+                    else {
+                        if (isVisible == true)
+                        {
+                            davis.Location = new Vector2(davis.Location.X, block.Location.Y - davis.HitBox.Height);
+                        }
+                        else {
+                            davis.Location = new Vector2(davis.Location.X, davis.Location.Y);
+                        }
+                        
+                    }
                     break;
                 case CollisionSide.Left:
                     davis.Location = new Vector2(block.Location.X - davis.HitBox.Width, davis.Location.Y);
                     break;
                 case CollisionSide.Right:
                     davis.Location = new Vector2(block.Location.X + block.HitBox.Width, davis.Location.Y);
-                    break;
-                case CollisionSide.Bottom:
-                    davis.Location = new Vector2(davis.Location.X, block.Location.Y + block.HitBox.Height);
-                    if(block is HiddenBlock)
-                    {
-                        block.SpecialState();
-                    }
-                    else if (block is QuestionBlock)
-                    {
-                        block.SpecialState();
-                    }else if (block is Brick)
-                    {
-                        block.SpecialState();
-                        block.Remove = true;
-                    }
                     break;
                 case CollisionSide.None:
                    // davis.DavisState.Static();
