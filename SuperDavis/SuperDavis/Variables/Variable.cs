20e7bef199499737c1c98616005cf719e6f4fce4
Ryan <Ryan@DESKTOP-CS6SR8V>
2019-06-18 12:42:06 -0400

    Added more level pieces.

--- SuperDavis/SuperDavis/Variables/Variable.cs
+++ SuperDavis/SuperDavis/Variables/Variable.cs
@@ -4,9 +4,7 @@
     {
         public const int WindowsEdgeWidth = 1024;
         public const int WindowsEdgeHeight = 768;
-
         public const int InvincibleTimer = 500;
-
         public const float SpriteScaleFactor = 1.5f;
     }
 }
