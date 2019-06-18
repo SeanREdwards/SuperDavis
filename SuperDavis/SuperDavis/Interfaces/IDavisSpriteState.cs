559996579f9034642b3a4e8afd22def4a3c6ff8f
xu.2460 <xu.2460@osu.edu>
2019-06-18 11:44:31 -0400

    Physics State done!

---
+++ SuperDavis/SuperDavis/Interfaces/IDavisSpriteState.cs
@@ -0,0 +1,15 @@
+﻿namespace SuperDavis.Interfaces
+{
+    interface IDavisSpriteState : IGameObjectSpriteState
+    {
+        new float Width { get; set; }
+        new float Height { get; set; }
+        void Static();
+        void Left();
+        void Right();
+        void Up();
+        void Down();
+        void Death();
+        void SpecialAttack();
+    }
+}
---
+++ SuperDavis/SuperDavis/PhysicsState/StandingState.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using SuperDavis.Interfaces;
+
+namespace SuperDavis.PhysicsState
+{
+
+    class JumpState : IGameObjectPhysicsState
+    {
+        // Idea, by passing different igameobject, implement different 
+        // param for jumping, using lists
+        private IGameObject gameObject;
+        private IDavis davis;
+        private float JumpVelocity;
+        private float JumpVelocityDecayRate;
+        private float JumpVelocityMin;
+        public JumpState(IGameObject gameObjectClass)
+        {
+            gameObject = gameObjectClass;
+            JumpVelocity = Variables.Variable.JumpVelocity;
+            JumpVelocityDecayRate = Variables.Variable.JumpVelocityDecayRate;
+            JumpVelocityMin = Variables.Variable.JumpVelocityMin;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            gameObject.Location -= new Vector2(0, JumpVelocity * (float)gameTime.ElapsedGameTime.TotalMilliseconds / 50);
+            JumpVelocity *= JumpVelocityDecayRate;
+            if (JumpVelocity < JumpVelocityMin)
+            {
+                JumpVelocity = 0;
+                gameObject.PhysicsState = new FallState(gameObject);
+            }
+        }
+    }
+}
