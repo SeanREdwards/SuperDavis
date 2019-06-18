dac637c2531abbe354e99f6161544aa110fdc988
xu.2460 <xu.2460@osu.edu>
2019-06-18 13:57:52 -0400

    Delete useless stuff

---
+++ SuperDavis/SuperDavis/Collision/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperDavis.Collision
+{
+    class Class1
+    {
+        //3432432432432
+    }
+}
