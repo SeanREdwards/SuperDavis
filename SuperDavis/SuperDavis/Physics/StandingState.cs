2524f5fc47279d9c3fa4732faf78271c2a14d551
xu.2460 <xu.2460@osu.edu>
2019-06-21 15:13:01 -0400

    Recall back to old physics to do changes

---
+++ SuperDavis/SuperDavis/Physics/StandingState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using SuperDavis.Interfaces;
+
+namespace SuperDavis.Physics
+{
+
+    class StandingState : IGameObjectPhysics
+    {
+        // Idea, by passing different igameobject, implement different 
+        // param for jumping, using lists
+        public StandingState(IGameObject gameObjectClass)
+        {
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}
