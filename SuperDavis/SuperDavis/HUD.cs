4d8a6c34a349c8304a3e631f50e591460652f46c
SeanREdwards <edwards.897@osu.edu>
2019-07-24 23:45:11 -0400

    Added CharacterSelect Precursor and calls to it in HUD file.

--- SuperDavis/SuperDavis/HUD.cs
+++ SuperDavis/SuperDavis/HUD.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using SuperDavis.Variables;
+using SuperDavis.Object.Character;
+using SuperDavis.Factory;
+
 
 namespace SuperDavis
 {
@@ -11,6 +14,8 @@ namespace SuperDavis
         public static int lives = Variable.lives;
         public static string worldText = Variable.worldText;
         public static double time = Variable.time;
+        private int x;
+        private static CharacterSelector characterSelector = new CharacterSelector();
 
         public static void Draw(GameTime gameTime, SpriteFont font, SpriteBatch spriteBatch)
         {
@@ -29,12 +34,32 @@ namespace SuperDavis
             spriteBatch.End();
         }
 
-        public static void DrawMenu(GameTime gameTime, SpriteFont font, SpriteBatch spriteBatch)
+        public static int DrawMenu(GameTime gameTime, SpriteFont font, SpriteBatch spriteBatch)
         {
             spriteBatch.Begin();
             spriteBatch.DrawString(font, "SuperDavis", new Vector2(50, 20), Color.White);
-            spriteBatch.DrawString(font, "Press Space To Start", new Vector2(50, 100), Color.White);
+            characterSelector.CharacterPortraitList[0].Draw(spriteBatch, new Vector2(300, 300));
+            characterSelector.CharacterPortraitList[1].Draw(spriteBatch, new Vector2(550, 300));
+            characterSelector.CharacterPortraitList[2].Draw(spriteBatch, new Vector2(800, 300));
+
+            characterSelector.NextCharacter(); //used for testing. Causing jitters atm.
+
+            if (characterSelector.GetCharacterId() == 0)
+            {
+                DavisSpriteFactory.Instance.CreateDavisWalkRightSprite().Draw(spriteBatch, new Vector2(350, 450));
+            }
+            else if(characterSelector.GetCharacterId() == 1)
+            {
+                DavisSpriteFactory.Instance.CreateWoodyWalkRightSprite().Draw(spriteBatch, new Vector2(600, 450));
+            }
+            else if (characterSelector.GetCharacterId() == 2)
+            {
+                DavisSpriteFactory.Instance.CreateBatWalkRightSprite().Draw(spriteBatch, new Vector2(850, 450));
+            }
+            //spriteBatch.DrawString(font, "Press Space To Start", new Vector2(50, 100), Color.White); //Original location per Ryan
+            spriteBatch.DrawString(font, "Press Space To Start", new Vector2(450, 550), Color.White);
             spriteBatch.End();
+            return 0;
         }
     }
 }
